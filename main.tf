7ca069df09d7ad052478e6ea494c81008bbea021
Aravind Koniki <aravind_koniki@hotmail.com>
2024-10-09 18:34:41 +0400

    updated IAM role code

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ resource "aws_iam_role" "my_dev_admin_role" {
 # Attach AdministratorAccess Policy
 resource "aws_iam_role_policy_attachment" "my_dev_admin_policy_attachment" {
   provider   = aws.MY_DEV_ENVIRONMENT
-  role       = aws_iam_role.admin_role.name
+  role       = aws_iam_role.my_dev_admin_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
